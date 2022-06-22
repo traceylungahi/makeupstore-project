7356149e66267511cf14c6be7470b779d8f810f7
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-22 11:53:13 +0300

    Use the external API to fetch data to populate the frontend

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const url = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybell
 fetch(url)
 .then(resp => resp.json())
 .then(data => {
-    console.log(data)
+    let html = " "
     data.map((items) => {
-
+        html += `
+        <div class="col-md-4">
+            <div class="card" style="width:330px">
+                <img class="card-img-top" src=${items.image_link} alt="Card image" style="width:100%">
+                <div class="card-body">
+                  <h4 class="card-title">${items.brand}</h4>
+                  <p class="card-text">${items.description}</p>
+                  <a href="#" class="btn btn-primary">${items.price}</a>
+                </div>
+              </div>
+        </div>
+        `
     })
+    let output = document.getElementById("item").innerHTML = html 
 })
+.catch((error) => console.log(error))
