7c0857027e5b1be9fb0454d87bcfc29ae261266c
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-22 16:25:37 +0300

    Use the CSS to style the page and make it look more appealing

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const url = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybell
 fetch(url)
 .then(resp => resp.json())
 .then(data => {
-    let html = " "
+    let body = " "
     data.map((items) => {
-        html += `
+        body += `
         <div class="col-md-4">
-            <div class="card" style="width:330px">
-                <img class="card-img-top" src=${items.image_link} alt="Card image" style="width:100%">
+            <div class="card" style="width:350px">
+                <img class="card-img-top" src=${items.image_link} alt="Card image" style="width:80%">
                 <div class="card-body">
-                  <h4 class="card-title">${items.brand}</h4>
+                  <h4 class="card-title text-dark">${items.brand}</h4>
                   <p class="card-text">${items.description}</p>
-                  <a href="#" class="btn btn-primary">${items.price}</a>
+                  <a href="#" class="btn btn-secondary">Price</a>
                 </div>
               </div>
         </div>
         `
+        document.getElementsByClassName("btn").addEventListener("click", () =>{
+        document.getElementsByClassName("btn").innerHTML = items.price
+        })
     })
-    let output = document.getElementById("item").innerHTML = html 
+    let output = document.getElementById("item").innerHTML = body 
+
 })
 .catch((error) => console.log(error))
